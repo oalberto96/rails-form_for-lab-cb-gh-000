38dab4c1f4e461471f96dfddba83f8b0a4dedf5f
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-23 23:16:43 +0000

    automatically backed up by learn

--- db/migrate/002_create_school_class.rb
+++ db/migrate/002_create_school_class.rb
@@ -1,3 +1,8 @@
-class CreateSchoolClass < ActiveRecord::Base 
-
+class CreateSchoolClass < ActiveRecord::Base
+  def change 
+    create_table do |t|
+      t.string :title 
+      t.integer :room_number
+    end
+  end
 end
